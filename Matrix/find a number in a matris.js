11c35f47ebd1441cfb681faa2c61c2608d1c7abe
a-rydzak <rydzaka12@gmail.com>
2018-06-27 12:06:32 -0500

    Morning Update

---
+++ Matrix/find a number in a matris.js
@@ -0,0 +1,45 @@
+//Test driven development practince
+
+// try { 
+// 	if(find(2) !=true ) throw "Test Not Passed";
+// }
+// catch(err) {
+//     console.log(err)  
+// }
+
+
+const M =[[1,2,3],
+	    [1,3,5],
+	    [2,7,8],
+	    [7,9,4]
+]
+ 
+if (find(2, M) !== true){
+	throw new Error('Check Fail')
+}
+
+if (find(10, M) !== false){
+	throw new Error('Check Fail 2')
+}
+
+if (find(10, M) !== false){
+	throw new Error('Check Fail 2')
+}
+
+
+
+function find(num, matrix){
+	for (var i=0; i<matrix.length; i++){
+		for(var j=0; j<matrix[i].length; j++){
+			if(matrix[i][j] === num){
+				return true
+			}
+		}
+	}
+	return false
+}
+
+
+
+
+
