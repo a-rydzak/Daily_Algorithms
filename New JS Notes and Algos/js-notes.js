41207711da23843bcaab82364e3efad360a5db5d
unknown <rydzaka12@gmail.com>
2020-01-01 17:07:14 -0600

    added callbacks

--- New JS Notes and Algos/js-notes.js
+++ New JS Notes and Algos/js-notes.js
@@ -1,25 +1,40 @@
 'use strict'
+const {performance} = require('perf_hooks');
 const list = [1, 2, 3, 4, 5, 6, 6, 6, 7]
 const set = new Set(list)
 
-console.log(set)
-
-//  rev a string
+//  reverse a string one method
 const rev = word => {
-
- word = word.split('')
-
- let last = ''
-
- for (let i = 0; i < word.length / 2; i++) {
-
-  last = word[word.length - i - 1]
-  word[word.length - i - 1] = word[i]
-  word[i] = last
- }
-
- word = word.join('')
- return word
+    word = word.split('')
+    let last = ''
+    for (let i = 0; i < word.length / 2; i++) {
+        last = word[word.length - i - 1]
+        word[word.length - i - 1] = word[i]
+        word[i] = last
+    }
+    word = word.join('')
+    return word
 }
 
-console.log(rev("its a brick"))
+//  reverse a string another method
+const rev2 = word =>{
+    let new_word = ''
+    for(let i = word.length-1; i>-1;i--){
+        new_word += word[i]
+    }
+    return new_word
+}
+
+
+const t1 = performance.now()
+rev2("its a brick")
+const t2 = performance.now()
+console.log(t2-t1)
+
+// callback
+
+const call = cb =>{
+    cb(33)
+}
+
+console.log(call(cb => cb))
