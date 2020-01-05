e969aa60c25c4eba7888c78c50bfdde877637b72
unknown <rydzaka12@gmail.com>
2020-01-05 16:51:06 -0600

    added two algos

--- New JS Notes and Algos/algos.js
+++ New JS Notes and Algos/algos.js
@@ -153,5 +153,91 @@ function averagePair(arr, avg){
     return false
   }
 
+  //   console.log(averagePair([1,2,3,4,5,6,9], 7.5))
 
-  console.log(averagePair([1,2,3,4,5,6,9], 7.5))
+  function isSubsequenceOne(str1, str2) {
+    for(let char of str1){
+        
+        if (!(str2.includes(char))) return false
+    }
+    return true
+  }
+
+  function isSubsequenceTwo(str1, str2) {
+    if(str1.length === 0) return true
+    if(str2.length === 0) return false
+    if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))  
+    return isSubsequence(str1, str2.slice(1))
+  }
+
+  function maxSubarraySum(arr, arrLen){
+
+    if(arr.length < arrLen) return null
+
+    let i = 0;
+    let largestSum = arr.slice(0,arrLen).reduce((a,b)=>a+b,0)
+    let largestArr = []
+    while(i<arr.length){
+        let newSum = arr.slice(i,i+arrLen).reduce((a,b)=>a+b,0)
+        if(newSum>largestSum){
+            largestArr = arr.slice(i,i+arrLen)
+            largestSum = newSum
+        }
+        i++
+    }
+    return largestSum
+    // add whatever parameters you deem necessary - good luck!
+  }
+
+//   console.log(isSubsequence('hello', 'hello world'))
+
+// console.log(maxSubarraySum([1,99,3,4,5,6],3))
+
+function minSubArrayLen(nums, sum) {
+    let total = 0;
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+   
+    while (start < nums.length) {
+      // if current window doesn't add up to the given sum then 
+          // move the window to right
+      if(total < sum && end < nums.length){
+        total += nums[end];
+              end++;
+      }
+      // if current window adds up to at least the sum given then
+          // we can shrink the window 
+      else if(total >= sum){
+        minLen = Math.min(minLen, end-start);
+              total -= nums[start];
+              start++;
+      } 
+      // current total less than required total but we reach the end, need this or else we'll be in an infinite loop 
+      else {
+        break;
+      }
+    }
+   
+    return minLen === Infinity ? 0 : minLen;
+  }
+console.log(minSubArrayLen([1,2,3,4,5,6], 7))
+
+
+function findLongestSubstring(str) {
+    let longest = 0;
+    let seen = {};
+    let start = 0;
+   
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+      if (seen[char]) {
+        start = Math.max(start, seen[char]);
+      }
+      // index - beginning of substring + 1 (to include current in count)
+      longest = Math.max(longest, i - start + 1);
+      // store the index of the next char so as to not double count
+      seen[char] = i + 1;
+    }
+    return longest;
+  }
