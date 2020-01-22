1afa6b5d6ca53b7345b550ad7f91f29f07deb336
unknown <rydzaka12@gmail.com>
2020-01-22 08:44:49 -0600

    added selection sort

---
+++ New JS Notes and Algos/selection-sort.py
@@ -0,0 +1,28 @@
+def main():
+
+    return selection_sort([1, 4, 3, 2, 5])
+
+
+def selection_sort(data):
+
+    
+    data_len = len(data)-1
+    smallest_index = 0
+
+    for j in range(data_len):
+        
+        smallest_val = data[j]
+
+        for i in range(j,data_len):
+
+            if data[i+1] < smallest_val:
+                smallest_val = data[i+1]
+                smallest_index = i+1
+
+        data[j], data[smallest_index] = smallest_val, data[j]
+
+    return data
+
+
+if __name__ == '__main__':
+    print(main())
