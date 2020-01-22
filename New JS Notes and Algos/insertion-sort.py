7f51f17d35fc065ac6cebafb4e6b305bf38d9fd3
unknown <rydzaka12@gmail.com>
2020-01-22 11:30:55 -0600

    finished insertion sort

--- New JS Notes and Algos/insertion-sort.py
+++ New JS Notes and Algos/insertion-sort.py
@@ -8,14 +8,15 @@ def insertion_sort(data):
 
         current = data[i]
 
-    while i>0 and data[i-1]>current:
-        data[i] = data[i-1]
-        i = i-1
-        data[i] = current
-    print(data)
+        while i>0 and data[i-1]>current:
+            data[i] = data[i-1]
+            i = i-1
+            data[i] = current
+            print(data)
 
 
     return data
 
 if __name__ == '__main__':
     print(main())
+
